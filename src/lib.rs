7132a522ae716bc8b142150f23d22a0d52f5da77
bruce.li <127000969+binary-bruce@users.noreply.github.com>
2024-09-01 11:38:43 +0800

    feat: put from_elf to MemorySet (#5)

--- src/lib.rs
+++ src/lib.rs
@@ -1,13 +1,11 @@
 #![no_std]
 
-mod elf;
 mod map_permission;
 mod map_type;
 mod memory_area;
 mod memory_set;
 mod memory_set_builder;
 
-pub use elf::from_elf;
 pub use map_permission::MapPermission;
 pub use map_type::MapType;
 pub use memory_area::MapArea;
