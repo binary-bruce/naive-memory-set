0968d45389e7f0cd62250221d2b0fc37d107ad17
binary.bruce <myth.litao@foxmail.com>
2024-09-01 11:51:10 +0800

    more utilities

--- src/memory_set.rs
+++ src/memory_set.rs
@@ -49,6 +49,22 @@ impl MemorySet {
         self.areas.push(map_area);
     }
 
+    pub fn remove_area_with_start_vpn(&mut self, start_vpn: VirtPageNum) {
+        if let Some((idx, area)) = self
+            .areas
+            .iter_mut()
+            .enumerate()
+            .find(|(_, area)| area.vpn_range.get_start() == start_vpn)
+        {
+            area.unmap(&mut self.page_table);
+            self.areas.remove(idx);
+        }
+    }
+
+    pub fn recycle_data_pages(&mut self) {
+        self.areas.clear();
+    }
+
     pub fn activate(&self) {
         let satp = self.page_table.token();
         unsafe {
